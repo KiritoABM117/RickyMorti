f3f68b05eb4fed6fcffbd48c33e05bc62426d3af
KiritoABM117 <adolbm67@gmail.com>
2022-10-07 13:13:12 -0500

    Commit Inicial

---
+++ main.js
@@ -0,0 +1,48 @@
+let d = document, $next = d.querySelector(".next"), $preview = d.querySelector('.preview');
+$img = d.querySelector('.img-cont'), $nombre = d.querySelector('.api-nombre'),
+$status = d.querySelector('.api-status'), $especie = d.querySelector('.api-especie');
+
+
+let page = 1;
+
+$next.addEventListener("click", e =>{
+    page++;
+    apiRick(page)
+})
+
+$preview.addEventListener("click", e =>{
+    if(page > 1){
+        page--;
+        apiRick(page);  
+    }
+})
+
+async function apiRick(page){
+    let url = `https://rickandmortyapi.com/api/character/${page}`;
+    
+    try {
+        let res = await fetch(url),
+        json = await res.json();
+        let apiRickMorty = json;
+        console.log(apiRickMorty);
+        $img.src = apiRickMorty.image;
+        $nombre.innerHTML = apiRickMorty.name;
+        $status.innerHTML = apiRickMorty.status;
+        $especie.innerHTML = apiRickMorty.species;
+
+
+    } catch (error) {
+        let message = error.statusText || "Ocurrio un error";
+    }
+}
+
+apiRick(page);
+
+
+
+
+
+
+
+
+
